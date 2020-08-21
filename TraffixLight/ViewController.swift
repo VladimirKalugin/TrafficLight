909a814dc024beb66a6e7af124b3b23a7c4266f2
Vladimir Kalugin <kaluginbc@gmail.com>
2020-08-21 13:03:55 +0700

    new repository

---
+++ TraffixLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TraffixLight
+//
+//  Created by Fuhrer_SS on 21.08.2020.
+//  Copyright © 2020 myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
