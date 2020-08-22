caedb9e3d2e920ac22d3b917fdc1b9b57464e0cc
Vladimir Kalugin <kaluginbc@gmail.com>
2020-08-22 12:10:03 +0700

    Make all of layouts func and ending view.

--- TraffixLight/ViewController.swift
+++ TraffixLight/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        startButton.layer.cornerRadius = 10
     }
 
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
 
+    @IBAction func buttonStartPresed(_ sender: Any) {
+        
+    }
+    
 }
 
